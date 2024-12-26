9e2d415a42d7fd78335f6ddbc5c6182c44bc187a
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2024-12-26 17:05:50 +0530

    Update install agent .sh

--- install agent .sh
+++ install agent .sh
@@ -1,75 +1,92 @@
-apiVersion: apps/v1
-kind: Deployment
+apiVersion: v1
+kind: Pod
 metadata:
-  name: {{ include "alloy-agent.fullname" . }}
+  name: {{ include "config-validator.fullname" . | quote }}
+  namespace: {{ .Release.Namespace }}
   labels:
-    {{- include "alloy-agent.labels" . | nindent 4 }}
+    app.kubernetes.io/managed-by: {{ .Release.Service | quote }}
+    app.kubernetes.io/instance: {{ .Release.Name | quote }}
+    app.kubernetes.io/version: {{ .Chart.AppVersion }}
+    helm.sh/chart: "{{ .Chart.Name }}-{{ .Chart.Version }}"
+    {{- range $key, $val := .Values.configValidator.extraLabels }}
+    {{ $key }}: {{ $val | quote }}
+    {{- end }}
+  annotations:
+    "helm.sh/hook": pre-install,pre-upgrade
+    "helm.sh/hook-weight": "-5"
+    "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
+    {{- range $key, $val := .Values.configValidator.extraAnnotations }}
+    {{ $key }}: {{ $val | quote }}
+    {{- end}}
 spec:
-  replicas: {{ .Values.replicaCount }}
-  selector:
-    matchLabels:
-      {{- include "alloy-agent.selectorLabels" . | nindent 6 }}
-  template:
-    metadata:
-      labels:
-        {{- include "alloy-agent.labels" . | nindent 8 }}
-    spec:
-      containers:
-        - name: {{ .Chart.Name }}
-          image: "{{ .Values.image.repository }}:{{ .Values.image.tag | default .Chart.AppVersion }}"
-          imagePullPolicy: {{ .Values.image.pullPolicy }}
-          command:
-            - /bin/sh
-            - -c
-            - |
-              # Check the package manager and install curl accordingly
-              if command -v apk > /dev/null; then
-                echo "Using apk to install curl"
-                apk update && apk add curl
-              elif command -v apt-get > /dev/null; then
-                echo "Using apt-get to install curl"
-                apt-get update && apt-get install -y curl && apt-get clean
-              else
-                echo "Unsupported package manager"
-                exit 1
-              fi
-
-              # Check if curl is installed
-              curl --version
-
-              set -ex
-              HOST_IP=$(hostname -I | awk '{print $1}')
-              response=$(curl -k -v -s -w "\n%{http_code}" -X POST "$API_ENDPOINT" \
-                  -H "Content-Type: application/json" \
-                  -d '{
-                      "host_name": "'"$HOSTNAME"'",
-                      "ip_port": "'"$HOST_IP:$ALLOY_PORT"'",
-                      "keycloak_id": "'"$OMEGA_UID"'",
-                      "agent_name": "'"$AGENT_NAME"'",
-                      "status": "Active",
-                      "cluster_name": "omega"  # Hardcoded cluster name as omega
-                  }')
-              http_code=$(echo "$response" | tail -n1)
-              body=$(echo "$response" | sed '$d')
-              if [[ "$http_code" == "201" ]]; then
-                echo "Agent created successfully."
-              else
-                echo "Agent creation failed. Response: $body"
-              fi
-          env:
-            - name: API_ENDPOINT
-              value: {{ .Values.apiEndpoint | quote }}
-            - name: ALLOY_CONFIG_URL
-              value: {{ .Values.alloyConfigUrl | quote }}
-            - name: ALLOY_PORT
-              value: "{{ .Values.alloyPort }}"
-            - name: HOSTNAME
-              valueFrom:
-                fieldRef:
-                  fieldPath: metadata.name
-            - name: OMEGA_UID
-              value: {{ .Values.env.omegaUid | quote }}
-            - name: AGENT_NAME
-              value: {{ .Values.env.agentName | quote }}
-            - name: CLUSTER_NAME
-              value: "omega"  # Using hardcoded cluster name
+  {{- if .Values.configValidator.serviceAccount.name }}
+  serviceAccountName: {{ .Values.configValidator.serviceAccount.name }}
+  {{- end }}
+  restartPolicy: Never
+  containers:
+    - name: alloy
+      image: "{{ .Values.global.image.registry | default (index .Values $enabledAlloy).image.registry }}/{{ (index .Values $enabledAlloy).image.repository }}{{ include "alloy.imageId" (index .Subcharts $enabledAlloy) }}"
+      command:
+      - bash
+      - -c
+      - |
+        echo Validating Grafana Alloy config file
+        if ! alloy fmt /etc/alloy/config.alloy > /dev/null; then
+          exit 1
+        fi
+        output=$(alloy run --stability.level {{ (index .Values $enabledAlloy).alloy.stabilityLevel | default "generally-available" }} "/etc/alloy/config.alloy" 2>&1)
+        if ! echo "${output}" | grep "KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined" >/dev/null; then
+          echo "${output}"
+          exit 1
+        fi
+        echo "Grafana Alloy config file is valid"
+{{- if .Values.logs.cluster_events.enabled }}
+        echo Validating Grafana Alloy for Events config file
+        if ! alloy fmt /etc/alloy/events.alloy > /dev/null; then
+          exit 1
+        fi
+        output=$(alloy run --stability.level {{ (index .Values "alloy-events").alloy.stabilityLevel | default "generally-available" }} "/etc/alloy/events.alloy" 2>&1)
+        if ! echo "${output}" | grep "KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined" >/dev/null; then
+          echo "${output}"
+          exit 1
+        fi
+        echo "Grafana Alloy for Events config file is valid"
+{{- end }}
+{{- if .Values.logs.pod_logs.enabled }}
+        echo Validating Grafana Alloy for Logs config file
+        if ! alloy fmt /etc/alloy/logs.alloy > /dev/null; then
+          exit 1
+        fi
+        output=$(alloy run --stability.level {{ (index .Values "alloy-logs").alloy.stabilityLevel | default "generally-available" }} "/etc/alloy/logs.alloy" 2>&1)
+        if ! echo "${output}" | grep "KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined" >/dev/null; then
+          echo "${output}"
+          exit 1
+        fi
+        echo "Grafana Alloy for Logs config file is valid"
+{{- end }}
+{{- if .Values.profiles.enabled }}
+        echo Validating Grafana Alloy for Profiles config file
+        if ! alloy fmt /etc/alloy/profiles.alloy > /dev/null; then
+          exit 1
+        fi
+        output=$(alloy run --stability.level {{ (index .Values "alloy-profiles").alloy.stabilityLevel | default "generally-available" }} "/etc/alloy/profiles.alloy" 2>&1)
+        if ! echo "${output}" | grep "KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined" >/dev/null; then
+          echo "${output}"
+          exit 1
+        fi
+        echo "Grafana Alloy for Profiles config file is valid"
+{{- end }}
+      env:
+        - name: KUBERNETES_SERVICE_HOST
+          value: ""
+        - name: KUBERNETES_SERVICE_PORT
+          value: ""
+        - name: CLUSTER_NAME  # Add the cluster name environment variable
+          value: "{{ .Values.cluster.name | quote }}"  # Reference the cluster name from values.yaml
+      volumeMounts:
+        - name: config
+          mountPath: /etc/alloy
+  volumes:
+    - name: config
+      configMap:
+        name: {{ include "config-validator.fullname" . | quote }}
